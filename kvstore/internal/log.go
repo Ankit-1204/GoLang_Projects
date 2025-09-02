3eef1c263012cf90b1e3a6ada786bc791302737d
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-09-02 17:28:06 +0530

    Implement append log in WAL

--- kvstore/internal/log.go
+++ kvstore/internal/log.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"encoding/gob"
 	"os"
 	"sync"
 )
@@ -12,16 +13,21 @@ type LogFile struct {
 }
 
 type Wal struct {
-	file  *os.File
-	entry *[]LogFile
-	lock  sync.Mutex
-	lsn   int64
+	file *os.File
+	lock sync.Mutex
+	lsn  int64
 }
 
-func (w *Wal) Append(log LogFile) {
+func (w *Wal) Append(log LogFile) error {
 	w.lock.Lock()
 	defer w.lock.Unlock()
 	log.LSN = w.lsn + 1
 	w.lsn = log.LSN
 
+	encoder := gob.NewEncoder(w.file)
+	if err := encoder.Encode(log); err != nil {
+		return err
+	}
+	// Actually commiting to the real file and not to a buffer
+	return w.file.Sync()
 }
