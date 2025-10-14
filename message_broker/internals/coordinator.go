a880eaa2e0100df64973583a0fe872696d3804a4
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-10-14 17:08:53 +0530

    producer and consumer listeners

--- message_broker/internals/coordinator.go
+++ message_broker/internals/coordinator.go
@@ -1,6 +1,7 @@
 package internals
 
 import (
+	"bufio"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -17,15 +18,36 @@ func Makecoordinator() *Coordinator {
 	return &c
 }
 
-func Publish(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		body, err := ioutil.ReadAll(r.Body)
-	}
+func Publish() {
+	
+}
+func Subscribe(){
+
 }
 func main() {
 	c := Makecoordinator()
-	ln, _ := net.Listen("tcp", "127.0.0.1:3333")
-	for {
+	sub, _ := net.Listen("tcp", "127.0.0.1:3333/sub")
+	pub, _ := net.Listen("tcp", "127.0.0.1:3333/pub")
+	go func(){
+		for {
+			subListener,err:=sub.Accept()
+			// sub will hold till it recieves request
+			go func(sublistener net.Conn){
+				subReader:=bufio.NewReader(subListener)
+				// basically \n so that it listens until first delim (line here)
+				topic,_:=reader.ReadString("\n") 
+				
+			}
+		}
+	}
+	go func(){
+		for{
+			pubListener,err:=pub.Accept()
+			go func(pubListener net.Conn){
+				pubReader:=bufio.NewReader(pubListener)
+				
 
+			}
+		}
 	}
 }
